6fa2506123ab3df11b16ca541c1bcce59bdafe26
Artem Tsys <19successful91@gmail.com>
2021-01-30 21:18:10 +0200

    add eslint

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-console.log("dfdf");
-console.log(_.last(["one", "two"]));
-const t = { t: 0, y: 99, l: "hfdhd" };
+console.log('dfdf');
+console.log(_.last(['one', 'two']));
